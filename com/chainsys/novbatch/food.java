12e5b1c269375deca37ed1f74d2713322663788d
raja3347 <raja3347@CSLH3138.global.chainsys.com>
2022-11-17 12:07:17 +0530

    file check

---
+++ com/chainsys/novbatch/food.java
@@ -0,0 +1,22 @@
+package com.chainsys.novbatch;
+
+public class food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String foodType="veg";
+		String cate="boiled";
+		String foodName="dosa";
+		int rate=5;
+		char ch='Y';
+		
+		System.out.println("The foodtype is:" + foodType);
+		System.out.println("A category of food:"+ cate);
+		System.out.println("The food name is:" + foodName);
+		System.out.println("The food Rate is:" + rate);
+		System.out.println("Like to for hotel:" +ch);
+
+	}
+
+}
