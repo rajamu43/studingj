63beb3ad5ab16ad5fa67f44fe227d102e6d82607
rajamu43 <118182509+rajamu43@users.noreply.github.com>
2023-04-10 13:02:44 +0530

    Add files via upload

---
+++ HospitalmanagementApplicationTests.java
@@ -0,0 +1,10 @@
+package com.hospital.hospitalmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalmanagementApplicationTests {
+
+
+}
